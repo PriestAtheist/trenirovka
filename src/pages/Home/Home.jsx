8603b3e13ecb8488ab526c4a36e39a70ccd4ce30
PriestAtheist <147350464+PriestAtheist@users.noreply.github.com>
2024-06-21 03:45:57 +0300

    Add files via upload

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Home.scss";
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <div className="header_page">
+                
+                <p>Главная</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
